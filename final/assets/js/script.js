90ee9b673750353666965f91dd4d6b924d4cdfe9
NickelCake <159098802+NickelCake@users.noreply.github.com>
2024-04-22 00:21:08 -0700

    finished final

--- final/assets/js/script.js
+++ final/assets/js/script.js
@@ -1,5 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll('.image');
+
+// showSlides(slideIndex);
+
+    images.forEach(image => {
+        image.addEventListener('mouseenter', () => {
+            image.style.transform = 'translateY(-10px)';
+        });
+
+        image.addEventListener('mouseleave', () => {
+            image.style.transform = 'translateY(0)';
+        });
+    });
+  showSlides(slideIndex);
+});
+
+
 let slideIndex = 1;
-showSlides(slideIndex);
+
 
 // Next/previous controls
 function plusSlides(n) {
@@ -14,17 +32,11 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  console.log(slides);
-  console.log(slideIndex);
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
+
